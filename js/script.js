be3ab086ef172d745c827ce5bd05fcc6c982c12e
nicolas <nicolas0978pzzz@gmail.com>
2026-07-18 21:55:48 -0500

    Insercion carrusel

--- js/script.js
+++ js/script.js
@@ -91,3 +91,24 @@ buscador.addEventListener("input", function () {
     contadorNoticias.textContent =
         `Noticias encontradas: ${visibles}`;
 });
+
+
+const slides = document.querySelectorAll(".slide");
+
+let indice = 0;
+
+function cambiarSlide() {
+
+    slides[indice].classList.remove("activo");
+
+    indice++;
+
+    if (indice >= slides.length) {
+        indice = 0;
+    }
+
+    slides[indice].classList.add("activo");
+
+}
+
+setInterval(cambiarSlide, 4000);
